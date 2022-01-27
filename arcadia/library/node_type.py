e532ace7fa67af876d17e8a1f9e7fc31dafa51aa
roboto84 <7658704+roboto84@users.noreply.github.com>
2022-01-27 07:39:54 -0500

    refactor(vine): restructure object record store

---
+++ arcadia/library/node_type.py
@@ -0,0 +1,7 @@
+
+import enum
+
+
+class NodeType(enum.Enum):
+    main = 1
+    sub = 2
--- arcadia/library/vine.py
+++ arcadia/library/vine.py
@@ -2,7 +2,8 @@
 import ast
 import sqlite3
 import logging.config
-from typing import Any
+from .node_type import NodeType
+from typing import Any, Optional
 
 
 class Vine:
@@ -11,22 +12,32 @@ class Vine:
         self._logger.setLevel(logging.INFO)
         self._vine: dict = {
             'subject': main_tag,
-            'nodes': []
+            'main_node': {},
+            'sub_node': []
         }
         self._records: list[dict] = []
         self._vine_sub_categories: list[str] = []
-        self._copy_records(records)
-        self._vine_sub_categories = list(set(self._vine_sub_categories))
-        self._vine_sub_categories.sort()
-        self._structure_vine(self._records)
+        if len(records) > 0:
+            self._copy_records(records)
+            self._vine_sub_categories = list(set(self._vine_sub_categories))
+            self._vine_sub_categories.sort()
+            self._structure_vine(self._records)
 
     def __str__(self):
         try:
-            categories_summaries: str = ''
-            for node in self._vine['nodes']:
-                categories_summaries += self._sub_category_summary(node)
-            return f'\n🌿  {self._vine["subject"].capitalize()}\n' \
-                   f'{categories_summaries}'
+            title: str = f'\n🌿  {self._vine["subject"].capitalize()}\n'
+            main_category_summary: str = ''
+            sub_category_summaries: str = ''
+            if len(self._records) == 0:
+                return f'{title} No results found\n'
+            else:
+                if self._vine['main_node']:
+                    main_category_summary += self._main_category_summary(self._vine['main_node'], NodeType.main)
+                for node in self._vine['sub_node']:
+                    sub_category_summaries += self._sub_category_summary(node, NodeType.sub)
+                return f'{title}' \
+                       f'{main_category_summary}\n' \
+                       f'{sub_category_summaries}'
         except TypeError as type_error:
             self._logger.error(f'Received type error outputting string representation: {str(type_error)}')
         except NameError as name_error:
@@ -37,7 +48,8 @@ class Vine:
             for record in records:
                 record_copy = dict(record).copy()
                 record_copy['tags'] = ast.literal_eval(record_copy['tags'])
-                record_copy['tags'].remove(self._vine['subject'])
+                if len(record_copy['tags']) != 1 and self._vine['subject'] in record_copy['tags']:
+                    record_copy['tags'].remove(self._vine['subject'])
                 self._vine_sub_categories = self._vine_sub_categories + record_copy['tags']
                 self._records.append(record_copy)
         except TypeError as type_error:
@@ -45,54 +57,64 @@ class Vine:
 
     def _structure_vine(self, records):
         try:
-            if len(self._vine_sub_categories) == 0:
-                new_node: dict = self._get_vine_node('self')
+            for sub_category in self._vine_sub_categories:
                 for record in records:
-                    if record['data_type'] == 'note':
-                        new_node['notes'].append(record)
-                    elif record['data_type'] == 'hyperlink':
-                        new_node['hyperlinks'].append(record)
-            else:
-                for sub_category in self._vine_sub_categories:
-                    for record in records:
-                        if sub_category in record['tags']:
-                            record['tags'].remove(sub_category)
-                            new_node: dict = self._get_vine_node(sub_category)
-                            if record['data_type'] == 'note':
-                                new_node['notes'].append(record)
-                            elif record['data_type'] == 'hyperlink':
-                                new_node['hyperlinks'].append(record)
+                    if sub_category in record['tags']:
+                        node_type = NodeType.sub
+                        if sub_category == self._vine['subject']:
+                            node_type = NodeType.main
+                        record['tags'].remove(sub_category)
+                        new_node: dict = self._get_vine_node(sub_category, node_type)
+                        if record['data_type'] == 'note':
+                            new_node['notes'].append(record)
+                        elif record['data_type'] == 'hyperlink':
+                            new_node['hyperlinks'].append(record)
 
         except TypeError as type_error:
             self._logger.error(f'Received error structuring vine: {str(type_error)}')
         except KeyError as key_error:
             self._logger.error(f'Received error structuring vine: {str(key_error)}')
 
-    def _get_vine_node(self, node_subject):
-        for node in self._vine['nodes']:
-            if node_subject == node['subject']:
-                return node
+    def _get_vine_node(self, node_subject, node_type: Optional[NodeType] = NodeType.sub):
+        if node_type == NodeType.main:
+            if self._vine['main_node']:
+                return self._vine['main_node']
+        elif node_type == NodeType.sub:
+            for node in self._vine['sub_node']:
+                if node_subject == node['subject']:
+                    return node
+
         new_node: dict = {
             'subject': node_subject,
             'notes': [],
             'hyperlinks': []
         }
-        self._vine['nodes'].append(new_node)
+        if node_type == NodeType.main:
+            self._vine['main_node'] = new_node
+        elif node_type == NodeType.sub:
+            self._vine['sub_node'].append(new_node)
         return new_node
 
-    def _sub_category_summary(self, node):
-        try:
-            return f'{node["subject"]}: \n' \
-                   f'{self._sub_category_details(node["notes"])}'\
-                   f'{self._sub_category_details(node["hyperlinks"])}'
-        except TypeError as type_error:
-            self._logger.error(f'Received type error creating category sub string: {str(type_error)}')
+    def _main_category_summary(self, node, node_type: NodeType):
+        return f'{self._category_summary(node, node_type)}'
 
-    def _sub_category_details(self, node_array: list[dict]):
+    def _sub_category_summary(self, node, node_type: NodeType):
+        return f'  {node["subject"]}: \n' \
+               f'{self._category_summary(node, node_type)}'
+
+    def _category_summary(self, node, node_type: NodeType):
+        try:
+            return f'{self._sub_category_details(node["notes"], node_type)}' \
+                   f'{self._sub_category_details(node["hyperlinks"], node_type)}'
+        except TypeError as type_error:
+            self._logger.error(f'Received type error creating sub category sub string: {str(type_error)}')
+
+    def _sub_category_details(self, node_array: list[dict], node_type: NodeType):
         try:
             if len(node_array):
+                spacer: str = '' if node_type == NodeType.main else '   '
                 return ''.join(
-                    f'  ◦ {item["time_stamp"]}{self.tag_string(item["tags"])}: {str(item["data"])}\n'
+                    f'{spacer}◦ {item["time_stamp"]}{self.tag_string(item["tags"])}: {str(item["data"])}\n'
                     for item in node_array
                 )
             else:
